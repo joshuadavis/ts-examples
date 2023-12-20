ce5cacc289e50a975abc6763387382a35a272d88
Joshua Davis <pgmjsd@gmail.com>
2023-12-20 08:52:16 -0500

    simple react app that displays the terrain

--- ex04/src/App.tsx
+++ ex04/src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import World from './world';
+import Grid from './grid';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const grid: Grid = new Grid();
+    const seed = 1234
+    const world = new World(grid, seed);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                Okay. seed={seed}
+            </header>
+            <div className="term-display">
+                {world.real_map_rows.map((row, index) =>
+                    <div key={index}>{row}</div>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ ex04/src/grid.ts
@@ -0,0 +1,55 @@
+
+export const DEFAULT_WIDTH = 100
+export const DEFAULT_HEIGHT = 60
+
+export default class Grid {
+
+    width: number
+    height: number
+    size: number        // computed
+    dirOffset: number[] // computed
+
+    constructor(width: number = DEFAULT_WIDTH,
+                height: number = DEFAULT_HEIGHT) {
+        this.height = height
+        this.width = width
+        this.size = width * height
+        this.dirOffset = [
+            -this.width, /* north */
+            -this.width + 1, /* northeast */
+            1, /* east */
+            this.width + 1, /* southeast */
+            this.width, /* south */
+            this.width - 1, /* southwest */
+            -1, /* west */
+            -this.width - 1 /* northwest */
+        ]
+    }
+
+    loc_col(loc: number): number {
+        return loc % this.width;
+    }
+
+    loc_row(loc: number): number {
+        return Math.floor(loc / this.width)
+    }
+
+    rc_loc(row:number, col:number) {
+        return row * this.width + col;
+    }
+
+    /**
+     * Weird distance calculation
+     *
+     * @param {number} a - The 'location' of the first point in the grid.
+     * @param {number} b - The 'location' of the second point in the grid.
+     * @return {number} The distance between the two points in the grid (according the game)
+     */
+    dist(a: number, b: number): number {
+        let ax = this.loc_row(a)
+        let ay = this.loc_col(a)
+        let bx = this.loc_row(b)
+        let by = this.loc_col(b)
+        return Math.max(Math.abs(ax - bx), Math.abs(ay - by))
+    }
+}
